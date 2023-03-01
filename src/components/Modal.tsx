7bfd7c6b8d81121e0f4d832836b201c5a16ac100
Antonios Zaravelas <antonis@Antonioss-Air.fritz.box>
2023-03-01 19:41:42 +0100

    create handleSubmit function for the submission of the form

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -2,6 +2,7 @@ import { faClose, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Modal from 'react-modal';
 import { Title } from '../typography/Headings';
+import { useState } from 'react';
 
 interface ModalComponentProps {
   modalIsOpen: boolean;
@@ -9,27 +10,41 @@ interface ModalComponentProps {
   closeModal: () => void;
 }
 
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    backgroundColor: '#18181B',
+    width: '70%',
+    height: '70%',
+  },
+};
+
+const modalInputStyle =
+  ' tracking-wider rounded-sm bg-gray-500 text-white py-1 pl-3  text-sm  border-none focus:outline-none w-1/2';
+
 const ModalComponent: React.FC<ModalComponentProps> = ({
   modalIsOpen,
   afterOpenModal,
   closeModal,
 }) => {
-  const customStyles = {
-    content: {
-      top: '50%',
-      left: '50%',
-      right: 'auto',
-      bottom: 'auto',
-      marginRight: '-50%',
-      transform: 'translate(-50%, -50%)',
-      backgroundColor: '#18181B',
-      width: '70%',
-      height: '70%',
-    },
-  };
+  const [formInfo, setFormInfo] = useState({
+    name: '',
+    username: '',
+    email: '',
+    phone: '',
+    address: '',
+    postTitle: '',
+    postContent: '',
+  });
 
-  const modalInputStyle =
-    ' tracking-wider rounded-sm bg-gray-500 text-white py-1 pl-3  text-sm  border-none focus:outline-none w-1/2';
+  const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+  };
 
   return (
     <Modal
@@ -51,6 +66,9 @@ const ModalComponent: React.FC<ModalComponentProps> = ({
             id="name"
             maxLength={60}
             className={modalInputStyle}
+            onChange={(event) =>
+              setFormInfo({ ...formInfo, name: event.target.value })
+            }
           />
         </div>
 
@@ -63,6 +81,9 @@ const ModalComponent: React.FC<ModalComponentProps> = ({
             id="username"
             maxLength={20}
             className={modalInputStyle}
+            onChange={(event) =>
+              setFormInfo({ ...formInfo, username: event.target.value })
+            }
           />
         </div>
 
@@ -70,39 +91,75 @@ const ModalComponent: React.FC<ModalComponentProps> = ({
           <label className="mr-2 text-white" htmlFor="email">
             Email:
           </label>
-          <input type="email" id="email" className={modalInputStyle} />
+          <input
+            type="email"
+            id="email"
+            className={modalInputStyle}
+            onChange={(event) =>
+              setFormInfo({ ...formInfo, email: event.target.value })
+            }
+          />
         </div>
 
         <div>
           <label className="mr-2 text-white" htmlFor="phone">
             Phone:
           </label>
-          <input type="phone" id="phone" className={modalInputStyle} />
+          <input
+            type="phone"
+            id="phone"
+            onChange={(event) =>
+              setFormInfo({ ...formInfo, phone: event.target.value })
+            }
+            className={modalInputStyle}
+          />
         </div>
 
         <div>
           <label className="mr-2 text-white" htmlFor="address">
             Address:
           </label>
-          <input type="text" id="address" className={modalInputStyle} />
+          <input
+            type="text"
+            id="address"
+            onChange={(event) =>
+              setFormInfo({ ...formInfo, address: event.target.value })
+            }
+            className={modalInputStyle}
+          />
         </div>
 
         <div>
           <label className="mr-2 text-white" htmlFor="postTitle">
             Post Title:
           </label>
-          <input type="text" id="postTitle" className={modalInputStyle} />
+          <input
+            type="text"
+            id="postTitle"
+            onChange={(event) =>
+              setFormInfo({ ...formInfo, postTitle: event.target.value })
+            }
+            className={modalInputStyle}
+          />
         </div>
 
         <div>
           <label className="mr-2 text-white" htmlFor="postContent">
             Post Content:
           </label>
-          <input type="text" id="postContent" className={modalInputStyle} />
+          <textarea
+            name="content"
+            id="postContent"
+            cols={30}
+            rows={10}
+            onChange={(event) =>
+              setFormInfo({ ...formInfo, postContent: event.target.value })
+            }
+          ></textarea>{' '}
         </div>
 
         <div>
-          <button className="bg-green-500 p-2 rounded">
+          <button onClick={handleSubmit} className="bg-green-500 p-2 rounded">
             <FontAwesomeIcon size="lg" className="mr-2" icon={faPaperPlane} />
             Post It!
           </button>
